db186ae87423e42d77f14213cb25108489a6ea47
shinyj1991 <63823237+shinyj1991@users.noreply.github.com>
2021-09-03 10:55:46 +0000

    Deploying to gh-pages from @ shinyj1991/shinyj1991.github.io@b12da7a2d19f448941f69e46a1e62a07d6d09330 🚀

--- _nuxt/252e18a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{470:function(e,n,t){"use strict";t.r(n);var l=t(1).a.extend({}),r=t(34),component=Object(r.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("Tutorial")}),[],!1,null,null,null);n.default=component.exports}}]);
--- _nuxt/static/1630644095/article/cate/1/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/article/cate/1", (function(a){return {data:[{article:{slug:"1",title:"블로그 글 테스트",author:"신용준",toc:[],body:{type:"root",children:[{type:"element",tag:"p",props:{},children:[{type:"text",value:"테스트입니다"}]}]},dir:"\u002Fcate",path:"\u002Fcate\u002F1",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2021-09-03T04:40:51.216Z")));
