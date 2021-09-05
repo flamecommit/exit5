118705f557038afe4255f1e6665a3339f3cc4f2e
shinyj1991 <63823237+shinyj1991@users.noreply.github.com>
2021-09-05 23:36:57 +0000

    Deploying to gh-pages from @ shinyj1991/shinyj1991.github.io@d61f331976f3c47552969b4f7090a2708d81bf74 🚀

--- _nuxt/static/1630810288/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630810288",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"445f1e78"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1630885016/article/cate/2/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/article/cate/2", (function(a){return {data:[{article:{slug:"2",title:"블로그 글 테스트",author:"신용준",toc:[],body:{type:"root",children:[{type:"element",tag:"p",props:{},children:[{type:"text",value:"테스트입니다"}]}]},dir:"\u002Fcate",path:"\u002Fcate\u002F2",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2021-09-05T23:35:55.253Z")));
