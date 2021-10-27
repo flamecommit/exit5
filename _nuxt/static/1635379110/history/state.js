9e709fb4603ed5406fd23c3830c082d5880cd4a2
shinyj1991 <63823237+shinyj1991@users.noreply.github.com>
2021-10-27 23:58:35 +0000

    Deploying to gh-pages from @ shinyj1991/shinyj1991.github.io@b4292f82208edb57af4a31a8085fa0ccab4ed1f5 🚀

---
+++ _nuxt/static/1635379110/history/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635379110",layout:"default",error:a,state:{app_load:b,params:c,id:c,example:{val:0}},serverRendered:b,routePath:"\u002Fhistory",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"21cae8e1"}}}}(null,true,void 0));
---
+++ _nuxt/static/1635379110/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635379110",layout:"default",error:a,state:{app_load:b,params:c,id:c,example:{val:0}},serverRendered:b,routePath:d,config:{_app:{basePath:d,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"21cae8e1"}}}}(null,true,void 0,"\u002F"));
