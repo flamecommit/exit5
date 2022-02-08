270f9af97fed71e277a307bc4acc444b426785de
shinyj1991 <63823237+shinyj1991@users.noreply.github.com>
2022-02-08 11:59:15 +0000

    Deploying to gh-pages from @ shinyj1991/shinyj1991.github.io@db76ab7249429f8052b4a907081403a6fa6939f1 🚀

---
+++ _nuxt/static/1644321550/vue/2021-11-03/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644321550",layout:"default",error:a,state:{is_menu:false,app_load:b,params:"vue",id:"2021-11-03",example:{val:0}},serverRendered:b,routePath:"\u002Fvue\u002F2021-11-03",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4459f84e"}}}}(null,true));
