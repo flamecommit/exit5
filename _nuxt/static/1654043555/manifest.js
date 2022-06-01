3cf61d48869b149e5b5e78056a0fd65ae836f7e2
shinyj1991 <63823237+shinyj1991@users.noreply.github.com>
2022-06-01 00:40:05 +0000

    Deploying to gh-pages from @ shinyj1991/shinyj1991.github.io@9be37079ee1126ae85b664e91772b90458cdeab1 🚀

--- _nuxt/static/1654043555/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fmusic\u002Flist","\u002Fproduct","\u002Fmusic\u002Fdetail\u002Fmusic_ballad_1","\u002Fmusic\u002Flist\u002Fmusic","\u002Fmusic\u002Flist\u002Fmusic_ballad","\u002Fmusic\u002Fdetail\u002Fmusic_folk_1","\u002Fmusic\u002Flist\u002Fmusic_folk"]})
