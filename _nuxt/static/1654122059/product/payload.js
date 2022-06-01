1e5641d4ec3ad72018d8f2649cde3d54f60c55b1
shinyj1991 <63823237+shinyj1991@users.noreply.github.com>
2022-06-01 23:36:26 +0000

    Deploying to gh-pages from @ shinyj1991/shinyj1991.github.io@a9975382c8269d397a343adf9a2488fd44e8c478 🚀

--- _nuxt/static/1654122059/product/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/product", (function(a,b,c){return {data:[{productId:a}],fetch:{"0":{category_list:[{name:b,path:b,depth:a,child:[{name:"ballad",path:"music_ballad",depth:c,child:[]},{name:"folk",path:"music_folk",depth:c,child:[]}]}]}},mutations:[]}}(0,"music",1)));
