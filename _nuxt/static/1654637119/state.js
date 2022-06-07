92577007abe1be08633a9cae39b739575a4de7ee
shinyj1991 <63823237+shinyj1991@users.noreply.github.com>
2022-06-07 21:25:21 +0000

    Deploying to gh-pages from @ shinyj1991/exit5-nuxt2@8eb3b87021094950278b87de6405bd92fdb1e11e 🚀

---
+++ _nuxt/static/1654637119/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654637119",layout:"default",error:a,state:{is_menu:b,app_loaded:c,is_loading:b,blog_depth:[]},serverRendered:c,routePath:d,config:{_app:{basePath:d,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5866f2ea"}}}}(null,false,true,"\u002F"));
