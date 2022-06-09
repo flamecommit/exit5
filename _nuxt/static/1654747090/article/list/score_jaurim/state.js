4935648415c9abd940f5b158b9ab48141afcb8fc
shinyj1991 <63823237+shinyj1991@users.noreply.github.com>
2022-06-09 03:58:12 +0000

    Deploying to gh-pages from @ shinyj1991/exit5-nuxt2@ae5be7ab741e0e0aef9e9b35ee456906c7ddcac1 🚀

---
+++ _nuxt/static/1654747090/article/list/score_jaurim/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654747090",layout:"default",error:a,state:{is_menu:false,app_loaded:b,blog_depth:["score","jaurim"]},serverRendered:b,routePath:"\u002Farticle\u002Flist\u002Fscore_jaurim",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e788082f"}}}}(null,true));
