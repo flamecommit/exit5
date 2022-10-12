70f9e950d4f9945aaa24564bee751f8064fde823
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-10-12 17:40:43 +0900

    feat: add viewport

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -6,6 +6,7 @@ const SEO = ({ title, description, url }) => {
   return (
     <Helmet>
       <title>{title}</title>
+      <meta name="viewport" content="width=768" />
       <meta name="author" content={author} />
       <meta name="description" content={description} />
 
